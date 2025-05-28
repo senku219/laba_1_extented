e5127e535004e4931564285e981d23f565ecc76a
mr-PeRforator <komputernyjila@gmail.com>
2025-05-28 10:55:41 +0300

    Создал форму для создания отчетов о сне, активности, питании

---
+++ HealthTrackerApp/ReportForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿// ReportForm.Designer.cs
+namespace HealthTrackerApp
+{
+    partial class ReportForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.RichTextBox rtbReport;
+        private System.Windows.Forms.Button btnClose;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.rtbReport = new System.Windows.Forms.RichTextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbReport
+            // 
+            this.rtbReport.Location = new System.Drawing.Point(12, 12);
+            this.rtbReport.ReadOnly = true;
+            this.rtbReport.Size = new System.Drawing.Size(360, 300);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(160, 320);
+            this.btnClose.Size = new System.Drawing.Size(80, 30);
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // ReportForm
+            // 
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.rtbReport);
+            this.Controls.Add(this.btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Отчёт о здоровье";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ HealthTrackerApp/ReportForm.cs
@@ -0,0 +1,38 @@
+﻿// ReportForm.cs
+using System;
+using System.Text;
+using System.Windows.Forms;
+using HealthTrackerApp.Models;
+
+namespace HealthTrackerApp
+{
+    public partial class ReportForm : Form
+    {
+        private readonly HealthReport _report;
+
+        public ReportForm(HealthTrackerApp.Models.HealthReport report)
+        {
+            _report = report;
+            InitializeComponent();
+            BuildReport();
+        }
+
+        private void BuildReport()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("=== Отчёт о здоровье ===\n");
+            sb.AppendLine("Активность:");
+            foreach (var kv in _report.ActivityTotals)
+                sb.AppendLine($"  {kv.Key}: {kv.Value} мин.");
+            sb.AppendLine("\nПитание:");
+            foreach (var kv in _report.NutritionTotals)
+                sb.AppendLine($"  {kv.Key}: {kv.Value} ккал.");
+            sb.AppendLine("\nСон:");
+            foreach (var kv in _report.SleepTotals)
+                sb.AppendLine($"  {kv.Key:dd.MM.yyyy}: {kv.Value} ч.");
+            rtbReport.Text = sb.ToString();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e) => Close();
+    }
+}
